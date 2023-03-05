89dd20322f74e24943dbf8ddd614651616e5c840
hawolt <45050975+hawolt@users.noreply.github.com>
2023-03-05 20:11:14 +0100

    removed unused static constact for league of legends

--- src/main/java/com/hawolt/oldseason/StaticConstants.java
+++ src/main/java/com/hawolt/oldseason/StaticConstants.java
@@ -7,7 +7,6 @@ package com.hawolt.oldseason;
 
 public class StaticConstants {
     public static final String RIOT_INSTALLS_JSON = "RiotClientInstalls.json";
-    public static final String LEAGUE_OF_LEGENDS = "League of Legends";
     public static final String SYSTEM_YAML = "system.yaml";
     public static final String RIOT_GAMES = "Riot Games";
 }
